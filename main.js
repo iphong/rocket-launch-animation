ba2b4be48e92dd540c21ba4993e854e38a0ddb63
Phong Vu <phong.vu@me.com>
2024-09-25 11:00:31 +0700

    Move code from local

---
+++ main.js
@@ -0,0 +1,142 @@
+void function () {
+	let canvas = document.querySelector('#canvas')
+	let rocket = document.querySelector('#rocket')
+
+	let mouseX = window.innerWidth / 2
+	let ctx = canvas.getContext('2d')
+
+	const cloudAnim = new Tween(3000, 0, 1, 'linear')
+	const rocketAnim = new Tween(5000, 200, 0, 'easeOutElastic')
+	// const smokeAnim = new Tween(7000, 0, -canvas.height * 5, 'linear')
+	const smokeAnim = new Tween(7000, 0, -canvas.height * 5, 'easeOutExpo')
+	// const smokeAnim = new Tween(7000, 0, -canvas.height * 5, 'easeOutQuad')
+
+	canvas.width = window.innerWidth
+	canvas.height = window.innerHeight
+
+	let rocketY = canvas.height - 200
+
+	let cloud = new Emitter(canvas, canvas.width / 2, canvas.height - 20, {
+		size: 75,
+		count: 2,
+		rate: 40,
+		speed: 1,
+		fade: 1,
+		angle: -90,
+		spread: 160,
+		bounceX: 1,
+		bounceY: -0.3,
+		windAngle: 90,
+		windSpeed: 0.18,
+		invert: 0
+	}).stop()
+	let smoke = new Emitter(canvas, canvas.width / 2, canvas.height / 2, {
+		size: 120,
+		count: 2,
+		rate: 30,
+		speed: 6,
+		fade: 7,
+		angle: 90,
+		spread: 15,
+		bounceX: 1,
+		bounceY: 1,
+		windAngle: 90,
+		windSpeed: 0,
+		invert: 1
+	}).stop()
+
+	function draw() {
+		let cloudTween = cloudAnim.tween
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+		smoke.y = rocketY + smokeAnim.tween + rocketAnim.tween
+		smoke.x = smoke.x * 0.97 + mouseX * 0.03
+		cloud.x = cloud.x * 0.97 + mouseX * 0.03
+
+		cloud.prop({
+			size: 60 + cloudTween * 250,
+			speed: 4 + cloudTween * 4,
+			rate: 70 + cloudTween * 70
+		})
+
+		cloud.loop()
+		smoke.loop()
+		
+		let tilt = (mouseX - smoke.x) / canvas.width * 45
+		
+		rocket.style.top = smoke.y + 'px'
+		rocket.style.left = smoke.x + 'px'
+		rocket.style.transform = `rotate3d(0, 0, 1, ${tilt}deg) translate3d(-50%, -110%, 0)`
+
+		requestAnimationFrame(draw)
+	}
+	
+	new Sequence({
+		0: () => {
+			
+		},
+		190: () => rocketAnim.start(),
+		200: () => smoke.start(),
+		// 300: () => cloud.start(),
+	})
+
+	let launched = false
+	window.onclick = function (e) {
+		if (launched) return
+		launched = true
+		new Sequence({
+			0: () => {
+				smoke.prop({
+					size: 250,
+					spread: 45,
+					count: 8,
+					speed: 7
+				})
+			},
+			900: () => {
+				cloud.prop({
+					windSpeed: 0.4,
+					bounceY: -0.1
+				})
+			},
+			1000: () => {
+				smoke.prop({
+					size: 100,
+					rate: 20,
+					spread: 8,
+					speed: 5,
+					count: 5
+				})
+				cloud.prop({
+					windSpeed: 0.05,
+					bounceY: -0.6,
+					fade: 5
+				})
+			},
+			1010: () => smokeAnim.start(),
+			1160: () => cloudAnim.start(),
+			1100: () => {
+				cloud.prop({
+					spread: 180,
+					fade: 1
+				})
+				canvas.classList.add('faded')
+			},
+		})
+	}
+	window.onresize = function (e) {
+		canvas.width = window.innerWidth
+		canvas.height = window.innerHeight
+		cloud.y = canvas.height - 20
+		rocketY = canvas.height - 180
+		mouseX = canvas.width / 4 + canvas.width / 2
+
+		// welcome[0].style.height = wHeight * 0.45 - 60 + 'px';
+	}
+	window.ontouchmove = function (e) {
+		mouseX = canvas.width / 4 + e.pageX / 2
+	}
+	window.onmousemove = function (e) {
+		mouseX = canvas.width / 4 + e.pageX / 2
+	}
+}()
---
+++ utils.js
@@ -0,0 +1,247 @@
+class Tween {
+	constructor(duration = 1000, from = 0, to = 1, easing = 'linear') {
+		this.from = from
+		this.to = to
+		this.duration = duration
+		this.easing = easing
+	}
+	start() {
+		this.time = Date.now()
+	}
+	get tween() {
+		if (!this.time) return this.from
+		var now = Date.now()
+		var lapsed = Math.min(now - this.time, this.duration)
+		return this.from + Easing[this.easing](lapsed / this.duration) * (this.to - this.from)
+	}
+}
+
+class Vector {
+	static deg2rad(deg) {
+		return deg * (Math.PI / 180)
+	}
+	static rad2deg(rad) {
+		return rad * (180 / Math.PI)
+	}
+	static fromAngle() {
+		var deg = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0]
+		var radius = arguments.length <= 1 || arguments[1] === undefined ? 1 : arguments[1]
+		return new Vector(radius * Math.cos(this.deg2rad(deg)), radius * Math.sin(this.deg2rad(deg)))
+	}
+	static randomRange(from, to) {
+		return from + Math.random() * (to - from)
+	}
+
+	constructor(x, y) {
+		this.x = x || 0
+		this.y = y || 0
+	}
+	add(v) {
+		if (v instanceof Vector) {
+			this.x += v.x
+			this.y += v.y
+		} else {
+			this.x += v
+			this.y += v
+		}
+		return this
+	}
+	subtract(v) {
+		if (v instanceof Vector) {
+			this.x -= v.x
+			this.y -= v.y
+		} else {
+			this.x -= v
+			this.y -= v
+		}
+		return this
+	}
+	multiply(v) {
+		if (v instanceof Vector) {
+			this.x *= v.x
+			this.y *= v.y
+		} else {
+			this.x *= v
+			this.y *= v
+		}
+		return this
+	}
+	divide(v) {
+		if (v instanceof Vector) {
+			this.x /= v.x
+			this.y /= v.y
+		} else {
+			this.x /= v
+			this.y /= v
+		}
+		return this
+	}
+	clone() {
+		return new Vector(this.x, this.y, this.z)
+	}
+}
+
+class Particle {
+	constructor(canvas = window.canvas, x = 0, y = 0, props = {}) {
+		this.props = props
+		this.canvas = canvas
+		this.context = canvas.getContext('2d')
+		this.life = 1
+		this.size = Vector.randomRange(props.size / 2, props.size)
+		this.angle = Vector.randomRange(props.angle - props.spread / 2, props.angle + props.spread / 2)
+		this.speed = Vector.randomRange(props.speed * 1.1, props.speed / 1.1)
+		this.pos = new Vector(x, y)
+		this.acc = new Vector()
+		this.vel = Vector.fromAngle(this.angle, this.speed)
+	}
+	setForce(force) {
+		this.acc = force
+	}
+	loop() {
+		this.life = Math.max(0, this.life - this.props.fade / 1000)
+		this.radius = this.size * (this.props.invert ? (1.05 - this.life) * 0.95 : this.life)
+		this.vel.add(this.acc)
+		if (this.pos.x > this.canvas.width || this.pos.x <= 0) this.vel.x *= this.props.bounceX || 0
+		if (this.pos.y > this.canvas.height || this.pos.y <= 0) this.vel.y *= this.props.bounceY || 0
+		this.pos.add(this.vel)
+		if (this.pos.x - this.size > this.canvas.width || this.pos.x + this.size <= 0) this.life = 0
+		if (this.pos.y - this.size > this.canvas.height) this.life = 0
+		this.render()
+	}
+	render() {
+		if (this.pos.y - this.size > this.canvas.height || this.pos.y + this.size <= 0) return this
+		this.context.beginPath()
+		this.context.arc(Math.round(this.pos.x), Math.round(this.pos.y), Math.round(this.radius), 0, 360)
+		this.context.fillStyle = 'rgba(255,255,255,1)'
+		this.context.fill()
+	}
+}
+
+class Emitter {
+
+	constructor(canvas = window.canvas, x = 0, y = 0, props = {}) {
+		this.props = {
+			size: 200,
+			count: 4,
+			rate: 50,
+			speed: 4,
+			fade: 3,
+			invert: 1,
+			angle: -90,
+			spread: 15,
+			bounceX: 1,
+			bounceY: 1,
+			windAngle: 0,
+			windSpeed: 0.03,
+			...props
+		}
+		this.pos = new Vector(x, y)
+		this.canvas = canvas
+		this.particles = []
+		this.lastTime = 0
+		this.context = canvas.getContext('2d')
+		this.emitting = true
+	}
+	prop(key, val) {
+		switch (true) {
+			case typeof key == 'object':
+				for (let k in key) {
+					this.prop(k, key[k])
+				}
+				break
+			default:
+				this.props[key] = val
+		}
+		return this
+	}
+	add() {
+		this.particles.push(new Particle(this.canvas, this.pos.x, this.pos.y, this.props))
+		return this
+	}
+	loop() {
+		this.particles = this.particles.filter(p => {
+			p.setForce(Vector.fromAngle(this.props.windAngle, this.props.windSpeed))
+			p.loop()
+			return p.life > 0
+		})
+		if (!this.emitting) return this
+		if (Date.now() - this.lastTime > this.props.rate) {
+			for (var i = 0; i < this.props.count; i++) {
+				this.add()
+			}
+			this.lastTime = Date.now()
+		}
+		return this
+	}
+	start() {
+		this.emitting = true
+		return this
+	}
+	stop() {
+		this.emitting = false
+		return this
+	}
+	get x() {
+		return this.pos.x
+	}
+	set x(value) {
+		return this.pos.x = value
+	}
+	get y() {
+		return this.pos.y
+	}
+	set y(value) {
+		return this.pos.y = value
+	}
+}
+
+var Easing = function () {
+	var easings = {
+		linear: function linear(p) {
+			return p
+		}
+	}
+	var baseEasings = {};
+	['Quad', 'Cubic', 'Quart', 'Quint', 'Expo'].forEach(function (name, i) {
+		baseEasings[name] = function (p) {
+			return Math.pow(p, i + 2)
+		}
+	})
+	Object.assign(baseEasings, {
+		Sine: function Sine(p) {
+			return 1 - Math.cos(p * Math.PI / 2)
+		},
+		Circ: function Circ(p) {
+			return 1 - Math.sqrt(1 - p * p)
+		},
+		Elastic: function Elastic(p) {
+			return p === 0 || p === 1 ? p : -Math.pow(2, 8 * (p - 1)) * Math.sin(((p - 1) * 80 - 7.5) * Math.PI / 15)
+		},
+		Back: function Back(p) {
+			return p * p * (3 * p - 2)
+		},
+		Bounce: function Bounce(p) {
+			var pow2, bounce = 4
+			while (p < ((pow2 = Math.pow(2, --bounce)) - 1) / 11) {
+			}
+			return 1 / Math.pow(4, 3 - bounce) - 7.5625 * Math.pow((pow2 * 3 - 2) / 22 - p, 2)
+		}
+	})
+	Object.keys(baseEasings).forEach(function (name) {
+		var easeIn = baseEasings[name]
+		easings['easeIn' + name] = easeIn
+		easings['easeOut' + name] = function (p) {
+			return 1 - easeIn(1 - p)
+		}
+		easings['easeInOut' + name] = function (p) {
+			return p < 0.5 ? easeIn(p * 2) / 2 : 1 - easeIn(p * -2 + 2) / 2
+		}
+	})
+	return easings
+}()
+
+function Sequence(actions) {
+	Object.entries(actions).forEach(([delay, action]) => {
+		setTimeout(action, parseInt(delay))
+	})
+}
